023ef33128243b5317889c597038be968487bc9b
aksamitksharma <aksharma@isystango.com>
2024-06-18 15:19:16 +0530

    create redux toolkit demo

---
+++ src/features/user/UserViews.jsx
@@ -0,0 +1,30 @@
+// eslint-disable-next-line no-unused-vars
+import React, {useEffect} from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { fecthUsers } from './UserSlice'
+
+export const UserViews = () => {
+    const dispatch = useDispatch()
+    const user = useSelector(state => state.user)
+    useEffect(() => {
+        dispatch(fecthUsers())
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
+
+  return (
+    <div>
+        <h2>List of users</h2>
+        {user.loading && <div>Loading...</div>}
+        {!user.loading && user.error ? <div>Error: {user.error}</div> : null}
+        {!user.loading && user.users.length?(
+            <ul>
+                {user.users.map(user=> (
+                    <li key={user.id}>{user.name}</li>
+                ))}
+            </ul>
+        ):null}
+    </div>
+
+  )
+}
